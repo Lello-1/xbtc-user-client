528cceaca189cca0eee7abcb6eeef8c199a843e9
Lello <ldeluca212@gmail.com>
2020-11-20 17:24:57 +0000

    Footer complete

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,5 +1,4 @@
 import './Sidebar.css';
-import arrow from '../../assets/arrow.png'
 
 const Sidebar = () => {
   return (
@@ -10,6 +9,13 @@ const Sidebar = () => {
       <a href="/user/dashboard">Deposit Bitcoin</a>
       <a href="/user/dashboard">Withdraw Bitcoin</a>
       <a href="/user/dashboard">Logout</a>
+
+      <footer>
+        <h3>Contact Us</h3>
+        <p>Tel: <span>07526 736 234</span></p>
+        <p className="footer_email">Email: <span>gfdbenade@gmail.com</span></p>
+        <small>&copy; Copyright 2020 XBTC. All Rights Reserved</small>
+      </footer>
     </div>
   );
 }
